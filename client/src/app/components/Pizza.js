478f871b1ec93cb0ba1064e627ab0e7a8e5a4b88
rrauf8178 <rrauf8178@gmail.com>
2024-07-11 19:31:54 +0400

    add: modal style and functionalities

--- client/src/app/components/Pizza.js
+++ client/src/app/components/Pizza.js
@@ -33,7 +33,7 @@ const Pizza = ({ pizza }) => {
   return <div className="group py-2 px-4 xl:py-4 xl:px-2 rounded-xl">
     <Image className="lg:group-hover:translate-y-3 transition-all duration-300 mb-8 cursor-pointer" width={270} height={270} src={pizza.image} alt='' priority={1} />
     {/* title */}
-    <div>
+    <div onClick={openModal}>
       <div className="text-xl font-bold mb-3 capitalize cursor-pointer">{pizza.name}</div>
     </div>
     {/* description */}
@@ -48,7 +48,17 @@ const Pizza = ({ pizza }) => {
       <button onClick={openModal} className="btn btn-sm gradient text-sm lg:hidden px-3">Starts at {pizza.priceSm}</button>
     </div>
     {/* modal */}
-    {modal && <Modal isOpen={modal} style={modalStyles} onRequestClose={closeModal} contentLabel="Pizza Modal">Modal</Modal>}
+    {modal && <Modal 
+    isOpen={modal} 
+    style={modalStyles} 
+    onRequestClose={closeModal} 
+    contentLabel="Pizza Modal" 
+    className='bg-white w-full h-full lg:max-w-[900px] lg:max-h-[600px] lg:rounded-[30px] lg:fixed lg:top-[50%] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[-50%] outline-none'>
+    {/* Close modal icon */}
+    <div onClick={closeModal} className="absolute z-30 right-2 top-2 hover:scale-110 duration-200 cursor-pointer">
+      <IoCloseOutline className="text-4xl text-orange"/>
+    </div>
+    </Modal>}
   </div>;
 };
 
