a1de814bb7e59c60d67542717f4721232d666a42
rrauf8178 <rrauf8178@gmail.com>
2024-08-05 14:14:43 +0400

    add: footer and finish project

--- client/src/app/components/Footer.js
+++ client/src/app/components/Footer.js
@@ -9,31 +9,35 @@ import Link from 'next/link'
 import { FaYoutube, FaFacebook, FaInstagram } from 'react-icons/fa'
 
 const Footer = () => {
-  return <footer className="bg-primary bg-pattern py-16">
-    <div className="container mx-auto">
-      <div className='flex flex-col items-center gap-y-6 justify-center'>
-        {/* logo */}
-        <Link href={"#"}><Image src={'logo.svg'} width={180} height={180} alt='' /></Link>
+  return (
+    <footer className="bg-primary bg-pattern py-16">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center gap-y-6 justify-center">
+          {/* logo */}
+          <Link href={'#'}>
+            <Image src={'logo.svg'} width={180} height={180} alt="" />
+          </Link>
 
-        {/* social icons */}
-        <div className='flex gap-x-6 text-xl text-white'>
-          <Link href={"#"}>
-            <FaYoutube />
-          </Link>
-          <Link href={"#"}>
-            <FaFacebook />
-          </Link>
-          <Link href={"#"}>
-            <FaInstagram />
-          </Link>
-        </div>
-        {/* copyright */}
-        <div className='text-white font-medium text-center'>
-          Copyright &copy: Pizzaland 2024. All rights reserved.
+          {/* social icons */}
+          <div className="flex gap-x-6 text-xl text-white">
+            <Link href={'#'}>
+              <FaYoutube />
+            </Link>
+            <Link href={'#'}>
+              <FaFacebook />
+            </Link>
+            <Link href={'#'}>
+              <FaInstagram />
+            </Link>
+          </div>
+          {/* copyright */}
+          <div className="text-white font-medium text-center">
+            Copyright &copy: Pizzaland 2024. All rights reserved.
+          </div>
         </div>
       </div>
-    </div>
-  </footer>
+    </footer>
+  )
 }
 
 export default Footer
